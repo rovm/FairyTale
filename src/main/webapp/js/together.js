70e83aeb1aea887cfa8c23f54ec43e322ae85345
조성규 <rovm92@naver.com>
2017-07-10 17:48:13 +0900

    '.'

--- src/main/webapp/js/together.js
+++ src/main/webapp/js/together.js
@@ -58,3 +58,29 @@ function hoverClass(fa, font, col, did) {
     }
   });
 }
+
+/* 모달  시작*/
+$(document).ready(function(){
+	  $("img").click(function(){
+		  console.log("하하")
+	  var t = $(this).attr("src");
+	  $(".modal-body").html("<img src='"+t+"' class='modal-img'>");
+	  $("#myModal").modal();
+	});
+
+	$("video").click(function(){
+	  var v = $("video > source");
+	  var t = v.attr("src");
+	  $(".modal-body").html("<video class='model-vid' controls><source src='"+t+"' type='video/mp4'></source></video>");
+	  $("#myModal").mosssdal();  
+	});
+	
+	 $(".video").click(function(){
+		  console.log("하하")
+	  var t = $("img").attr("src");
+	  $(".modal-body").html("<img src='"+t+"' class='modal-img'>");
+	  $("#myModal").modal();
+	});
+
+	});//EOF Document.ready
+	
