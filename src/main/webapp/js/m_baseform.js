b9c92138d934010736d4ef2f0aef8b9a540448ec
rovm <rovm92@naver.com>
2017-07-13 10:56:12 +0900

    .

--- src/main/webapp/js/m_baseform.js
+++ src/main/webapp/js/m_baseform.js
@@ -14,9 +14,20 @@ $(".mobile-one .menu-toggle, .mobile-three .menu-toggle").click(function() {
 
 });
 
+function headerBtn() {
+    var menu_btn= document.getElementById('menubar');
+    if (menu_btn.style.display == 'none') {
+    	menu_btn.style.display = 'block';
+    } else {
+    	menu_btn.style.display = 'none';
+    }
+}
+
 function communityBtn() {
-    var com_btn= document.getElementById('community-btn');
+    var com_btn= document.getElementById('communityBtn');
+    var service_btn= document.getElementById('serviceBtn');
     if (com_btn.style.display == 'none') {
+    	service_btn.style.display = 'none';
         com_btn.style.display = 'block';
     } else {
         com_btn.style.display = 'none';
@@ -24,10 +35,19 @@ function communityBtn() {
 }
 
 function serviceBtn() {
-    var com_btn= document.getElementById('service-center-btn');
-    if (com_btn.style.display == 'none') {
-        com_btn.style.display = 'block';
+	var com_btn= document.getElementById('communityBtn');
+    var service_btn= document.getElementById('serviceBtn');
+    if (service_btn.style.display == 'none') {
+    	com_btn.style.display = 'none';
+    	service_btn.style.display = 'block';
     } else {
-        com_btn.style.display = 'none';
+    	service_btn.style.display = 'none';
     }
+}
+
+function menu_reset(){
+  var com_btn= document.getElementById('communityBtn');
+  var service_btn= document.getElementById('serviceBtn');
+  service_btn.style.display = 'none';
+  com_btn.style.display = 'none';
 }
