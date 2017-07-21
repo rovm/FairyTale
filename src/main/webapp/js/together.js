893bb41c285a6b082f9047a990d4e50c08f95e71
조성규 <rovm92@naver.com>
2017-07-21 18:15:05 +0900

    '.'

--- src/main/webapp/js/together.js
+++ src/main/webapp/js/together.js
@@ -5,6 +5,7 @@ var sec = true;
 hoverClass("", "#com-font", "orange", "#com-king-s")
 hoverClass("", "#com-font2", "orange", "#com-together-s")
 hoverClass("", "#com-font3", "orange", "#com-bord-s")
+lengthTest(); //div 다 안차면 버튼 회색으로 만듬
 
 
 function hoverClass(fa, font, col, did) {
@@ -62,7 +63,6 @@ function hoverClass(fa, font, col, did) {
 /* 모달  시작*/
 $(document).ready(function(){
 	  $("img").click(function(){
-		  console.log("하하")
 	  var t = $(this).attr("src");
 	  $(".modal-body").html("<img src='"+t+"' class='modal-img'>");
 	  $("#myModal").modal();
@@ -74,40 +74,128 @@ $(document).ready(function(){
 	  $(".modal-body").html("<video class='model-vid' controls><source src='"+t+"' type='video/mp4'></source></video>");
 	  $("#myModal").mosssdal();
 	});
+	
+	$('#d').val()
+/*모달 끝*/
+	
+	//버튼 이벤트 시작
+	var i = 1
+	$("#next-btn").click(function(){
+		$("#d").empty()
+		       .append(++i);
+		$("#video-box").empty()
+		json2();
+		lengthTest();
+	})
+	
+	$("#prev-btn").click(function(){
+		if (i != 1) {
+		  $("#d").empty()
+			     .append(--i);
+	      $("#video-box").empty()
+		  json();
+		}
+		
+		lengthTest();
+	})
+    
+	//버튼 이벤트 끝
+});
 
-	 $(".v1").click(function(){
-		  console.log("하하")
-	  var t = $("#i1").attr("src");
-	  $(".modal-body").html("<img src='"+t+"' class='modal-img'>");
-	  $("#myModal").modal();
-	});
+/*얘도 모달*/
+function modalImg(no) {
+	console.log(no)
+	var t = $(no).attr("src");
+	$(".modal-body").html("<img src='"+t+"' class='modal-img'>");
+	$("#myModal").modal();
+}
 
-	 $(".v2").click(function(){
-		  console.log("하하")
-	  var t = $("#i2").attr("src");
-	  $(".modal-body").html("<img src='"+t+"' class='modal-img'>");
-	  $("#myModal").modal();
-	});
+//0.1초뒤에 버튼 회색!
+function lengthTest() {
+	setTimeout(() => {
+		console.log($(".video").length)
+		if ($(".video").length != "5"){
+			console.log($(".video").length)
+			$("#next-btn").attr("disabled", "true")
+			              .css("background-color", "darkgray");
+		} else if ($(".video").length == 5) {
+			$("#next-btn").removeAttr("disabled", "true")
+			              .css("background-color", "");
+		}
+	}, 100);	
+	
+}
 
-	 $(".v3").click(function(){
-		  console.log("하하")
-	  var t = $("#i3").attr("src");
-	  $(".modal-body").html("<img src='"+t+"' class='modal-img'>");
-	  $("#myModal").modal();
-	});
 
-	 $(".v4").click(function(){
-		  console.log("하하")
-	  var t = $("#i4").attr("src");
-	  $(".modal-body").html("<img src='"+t+"' class='modal-img'>");
-	  $("#myModal").modal();
-	});
-
-	 $(".v5").click(function(){
-		  console.log("하하")
-	  var t = $("#i5").attr("src");
-	  $(".modal-body").html("<img src='"+t+"' class='modal-img'>");
-	  $("#myModal").modal();
-	});
-
-	});//EOF Document.ready
+//JSON
+json()
+function json() {
+	
+	$.getJSON('../js/together.json', function(data) {
+		
+		var testData = {
+				div : [ 
+					{ 
+						imgId : data.div1.imgId, 
+						srcImg : data.div1.srcImg, 
+						title : data.div1.title, 
+						content : data.div1.content,
+						date : data.div1.date
+					},
+					{ 
+						imgId : data.div2.imgId, 
+						srcImg : data.div2.srcImg, 
+						title : data.div2.title, 
+						content : data.div2.content,
+						date : data.div2.date
+					},
+					{ 
+						imgId : data.div3.imgId, 
+						srcImg : data.div3.srcImg, 
+						title : data.div3.title, 
+						content : data.div3.content,
+						date : data.div3.date
+					},
+					{ 
+						imgId : data.div4.imgId, 
+						srcImg : data.div4.srcImg, 
+						title : data.div4.title, 
+						content : data.div4.content,
+						date : data.div4.date
+					},
+					{ 
+						imgId : data.div5.imgId, 
+						srcImg : data.div5.srcImg, 
+						title : data.div5.title, 
+						content : data.div5.content,
+						date : data.div5.date
+					}
+					]
+		} 
+		var template = Handlebars.compile($('#template').html());
+		var html = template(testData);
+		$("#video-box").append(html);
+		
+		console.log(template);
+		console.log(html);
+	})
+}
+function json2() {
+	$.getJSON('../js/together.json', function(data) {
+		var testData = {
+				div : [ 
+					{ 
+						"imgId" : data.div11.imgId, 
+						srcImg : data.div11.srcImg, 
+						title : data.div11.title, 
+						content : data.div11.content,
+						date : data.div11.date
+					}
+					]
+		}
+		
+		var template = Handlebars.compile($('#template').html());
+		var html = template(testData);
+		$("#video-box").append(html);
+	})	
+}
