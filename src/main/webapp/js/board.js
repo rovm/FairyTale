76c0f6f7728dda7fae4370261c2b7617a2fbb1a0
ksw529 <qntn147@gamil.com>
2017-08-02 11:34:09 +0900

    .

--- src/main/webapp/js/board.js
+++ src/main/webapp/js/board.js
@@ -1,24 +1,64 @@
-$(document).ready(function(){
-  $("#keyword").keyup(function(){
-      var k = $(this).val();
-      $(".board").hide();
-      var temp = $(".board > td:nth-child(2n):contains('" + k + "')");
+var word = $("#keyword");
 
-      temp.parent().show();
-  })
-})
-/*****************************************************/
-$('#bord-btn').click(function(){
-	location.href = 'boardwrite.html'
-})
 
-/******************************************************/
+  
 var pageNoTag = $('#page-no')  // 매번 이걸 찾으면 안좋다 찾아놓은걸 쓰자
     tbody = $('table > tbody'),
     prevBtn = $('#prev-btn'),
     nextBtn = $('#next-btn'),
     currPageNo = $('#page-no')
     pageSize = 10;
+
+$("#keyword").keyup(function(){
+//	$.post('suchList.json', {
+//		'keyword': word.val()
+//	}, function(result) {
+//	}, 'json')
+	displayList2(1);
+})
+
+displayList(1);
+function displayList2(pageNo){
+	// 서버에서 강사 목록 데이터를 받아 온다.
+  $.getJSON('suchList.json', {'pageNo':pageNo, 'pageSize':pageSize , 'keyword':word.val()}, function(result) {// url, 서버에 보낼 데이터, 서버에서 받을 함수 비동기 방식
+  var totalCount = result.data.totalCount
+  console.log("이거",result.data)
+  var lastPageNo = parseInt(totalCount / pageSize) + (totalCount % pageSize > 0 ? 1: 0)
+
+  var templateFn = Handlebars.compile($('#tbody-template').text())
+  var generatedHTML = templateFn(result.data)
+    tbody.text('')
+    tbody.html(generatedHTML)  // 문자열 html을 리턴한다.
+    
+    currPageNo = pageNo
+    pageNoTag.text(currPageNo)
+    console.log(result)
+    
+    if(currPageNo == 1){
+      prevBtn.prop('disabled', true)
+    } else {
+      prevBtn.prop('disabled', false)
+    }
+
+    if(currPageNo == lastPageNo){
+      nextBtn.prop('disabled', true)
+    } else {
+      nextBtn.prop('disabled', false)
+    }
+    console.log("어디서 안되는거야2", templateFn);
+    
+  })
+}
+
+
+
+/*****************************************************/
+$('#bord-btn').click(function(){
+	location.href = 'boardwrite.html'
+})
+
+/******************************************************/
+
 
 var currPageNo = parseInt(pageNoTag.text())
 $(document.body).on('click', '.detail-link', function(event){
@@ -38,7 +78,8 @@ nextBtn.click(function() {
 function displayList(pageNo){
 	// 서버에서 강사 목록 데이터를 받아 온다.
   $.getJSON('list.json', {'pageNo':pageNo, 'pageSize':pageSize}, function(result) {// url, 서버에 보낼 데이터, 서버에서 받을 함수 비동기 방식
-  var totalCount = result.data.totalCount
+
+	  var totalCount = result.data.totalCount
   console.log(result.data)
   var lastPageNo = parseInt(totalCount / pageSize) + (totalCount % pageSize > 0 ? 1: 0)
 
@@ -49,9 +90,6 @@ function displayList(pageNo){
     
     currPageNo = pageNo
     pageNoTag.text(currPageNo)
-    console.log(totalCount)
-    console.log(lastPageNo)
-    console.log(result)
     if(currPageNo == 1){
       prevBtn.prop('disabled', true)
     } else {
@@ -63,12 +101,12 @@ function displayList(pageNo){
     } else {
       nextBtn.prop('disabled', false)
     }
-    
+    console.log("어디서 안되는거야1", generatedHTML);
     
 console.log(result)
   })
 }
-displayList(1)
+//displayList(1)
 
 /************************************************/
 
