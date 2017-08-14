48a7f77e1819a72cf88361f91ce8226bb7d05acb
ksw529 <qntn147@gamil.com>
2017-08-14 15:24:25 +0900

    .
    .

--- src/main/webapp/js/kakaotalk2.js
+++ src/main/webapp/js/kakaotalk2.js
@@ -5,41 +5,42 @@ Kakao.init('eaeddfa1ade8ed3a16075bd54bb6f4b6');
         success: function(authObj) {
         	console.log(authObj)
         	kakao.API.request({
-        		url: 'main.html'
+        		url: 'main.html',
         		success: function(res){
         			console.log(JSON.stringify(res));
 					console.log(res.id)
-					
-						$.post('login2.json', {
-							'id' : res.id,
-							'password' : res.id
-						}, function(result) {
-							console.log(res.id)
-							console.log('로그인result:')
-							console.log(result)
-							
-							if (result.data == 'ok') {
-							    location.href = 'main.html'
-							   } else {
-				                $.post('//add.json', {'id': res.id, 'password': res.id, 'loginType': 'kakaoTalk'}, function(){console.log("okok")/*})*/
-				                $.post('/auth/login.json', {
-				                	'id': res.id,
-								    'password': res.id
-				                  }, function(result) {
-				                    console.log('result가 fail일 때의 로그인result:')
-				                    console.log(result)
-				                    if (result.data == 'ok') {
-				                    location.href = 'web/movieperson/mypage.html'
-				                   } else {
-				                	   console.log(result)
-				                	   alert('kakaotalk 로그인 아니됩니다!')
-				                   }
-				                  }, 'json') })//add.json
-							   }
-							  }, 'json')  //login.json 
+//					
+//						$.post('login2.json', {
+//							'id' : res.id,
+//							'password' : res.id
+//						}, function(result) {
+//							console.log(res.id)
+//							console.log('로그인result:')
+//							console.log(result)
+//							
+//							if (result.data == 'ok') {
+//							    location.href = 'main.html'
+//							   } else {
+//				                $.post('//add.json', {'id': res.id, 'password': res.id, 'loginType': 'kakaoTalk'}, function(){console.log("okok")/*})*/
+//				                $.post('/auth/login.json', {
+//				                	'id': res.id,
+//								    'password': res.id
+//				                  }, function(result) {
+//				                    console.log('result가 fail일 때의 로그인result:')
+//				                    console.log(result)
+//				                    if (result.data == 'ok') {
+//				                    location.href = 'web/movieperson/mypage.html'
+//				                   } else {
+//				                	   console.log(result)
+//				                	   alert('kakaotalk 로그인 아니됩니다!')
+//				                   }
+//				                  }, 'json') })//add.json
+//							   }
+//							  }, 'json')  //login.json 
 						
 					},
 					fail: function(error) {
 						console.log(JSON.stringify(error));
 					}
 				});
+}
